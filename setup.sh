9af3c615d37827b6d0e3a9b905c4b07f402f8cd2
Johannes Porzelt <5166883+jporzelt@users.noreply.github.com>
2023-10-26 16:15:32 +0200

    Update setup.sh
    removed docker-compose

--- setup.sh
+++ setup.sh
@@ -31,9 +31,6 @@ sudo sh get-docker.sh
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 # rambox pro
 sh ./rambox-update.sh
 
